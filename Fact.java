33313e8dda6212f38197a85963c0e00246cc85bf
MEENA1804 <meenanandy111@gmail.com>
2017-07-28 16:46:18 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,17 @@
+package meena;
+import java.io.*;
+public class Fact {
+	 public static void main(String[] args) {
+
+	        int num = 10;
+	        long factorial = 1;
+	        for(int i = 1; i <= num; ++i)
+	        {
+	            // factorial = factorial * i;
+	            factorial *= i;
+	        }
+	        System.out.printf("Factorial of %d = %d", num, factorial);
+	    }
+	}
+
+
