c5415d71f2618f4f22caae79ecb1ea0322ee836a
MEENA1804 <meenanandy111@gmail.com>
2017-08-07 15:09:02 +0530

    Add files via upload

---
+++ Upper.java
@@ -0,0 +1,23 @@
+package guvi;
+import java.util.Scanner;
+	public class Upper{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = scanner.nextLine();
+        String result = "";
+        char firstChar = input.charAt(0);
+        result = result + Character.toUpperCase(firstChar);
+        for (int i = 1; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char previousChar = input.charAt(i - 1);
+            if (previousChar == ' ') {
+                result = result + Character.toUpperCase(currentChar);
+            } else {
+                result = result + Character.toLowerCase(currentChar);
+            }
+        }
+        System.out.println(result);
+    }
+}
+
